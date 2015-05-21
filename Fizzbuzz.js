44f47c7faeb5ec763ad7c90728ab818d2034213f
lostje <anze.anzic@siol.net>
2015-05-21 22:11:19 +0200

    Final version: - added promise - added factory - data display modification

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,30 +1,39 @@
 angular.module('Fizzbuzz', []);
 
-angular.module('Fizzbuzz').controller('FizzbuzzController', function($scope) {
+angular.module('Fizzbuzz').controller('FizzbuzzController', function($scope, FizzbuzzFactory) {
+	$scope.fizzbuzz = FizzbuzzFactory;
 
-	$scope.fizzbuzz = function(number) {
-		$scope.result = "";
-		number = parseInt(number);
-
-		if (number && number > 0) {
-			for (var i = 1; i <= number; i++) {
-				if ((i % 3 === 0) && (i % 5 === 0)) {
-					$scope.result += " FizzBuzz";
-				}
-				else if (i % 3 === 0) {
-					$scope.result += " Fizz";
-				}
-				else if (i % 5 === 0) {
-					$scope.result += " Buzz";
-				}
-				else {
-					$scope.result += " "+i;
-				}
-			}
-		}
-		else {
-			$scope.result = "Enter a number ...";
-		}
+	$scope.startFizzbuzz = function(number) {
+		$scope.fizzbuzz.displayNumbers(number).then(function(success) {
+			$scope.result = success;
+		}, function(error) {
+			$scope.result = error;
+		});
 	}
+});
 
+angular.module('Fizzbuzz').factory('FizzbuzzFactory', function($q) {
+	return {
+		displayNumbers: function(number) {
+			var defer = $q.defer();	
+
+			if (number && number > 0) {
+				var string = "";
+
+				for (var i = 1; i <= number; i++) {
+					var fizz = (i % 3 === 0);
+					var buzz = (i % 5 === 0);
+					string += (fizz ? buzz ? " fizzbuzz" : " fizz" : buzz ? " buzz" : " "+i);
+					string += "\n";
+				}
+
+				defer.resolve(string);
+			}
+			else {
+				defer.reject("The entered value is not a positive number. Please enter a positive number ...");
+			}
+
+			return defer.promise;
+		}
+	};
 });
